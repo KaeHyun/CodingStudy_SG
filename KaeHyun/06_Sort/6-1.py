6dee87b09323b681d65066c4b3f3edd4261bcc8c
KaeHyun <80453200+KaeHyun@users.noreply.github.com>
2023-07-17 00:25:27 +0900

    Revert "sort"
    This reverts commit c474bf0f032162ee8e219f9a55374dc59bf2e29a.

--- KaeHyun/06_Sort/6-1.py
+++ KaeHyun/06_Sort/6-1.py
@@ -1,7 +1,5 @@
 # 선택 정렬 알고리즘 : 가장 작은 값을 선택해서 맨 앞으로 보내는 방식
 
-# 선택 정렬의 시각복잡도는? O(n^2)
-
 array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
 
 for i in range(len(array)):
@@ -16,13 +14,3 @@ for i in range(len(array)):
     
 
 print(array)
-
-'''
-[ 파이썬 Swap 코드 ]
-
-array = [5, 3]
-array[0], array[1] = array[1], array[0]
-
-간단한 스와프는 위 코드를 활용하자! 
-
-'''
